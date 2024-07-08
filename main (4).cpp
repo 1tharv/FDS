0e8071cd8c59c7acf20a03d3d2d2ed2de8b1edad
Atharv <58655568+1tharv@users.noreply.github.com>
2024-07-08 23:40:29 +0530

    Even Odd

---
+++ main (4).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    
+    cout<<"Input: ";
+    cin>>num;
+    
+    if(num%2==0)
+    {
+        cout<<"Given number is Even";
+    }
+    
+    else
+    {
+        cout<<"Given Number is Odd";
+    }
+
+    return 0;
+}
